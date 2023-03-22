5e8857a52d9af9cd15f8546843cc78799b36684d
jwPark <1211abc@naver.com>
2023-03-22 20:32:01 +0900

    feat : [3] Login[Local] xml 구현

--- app/src/main/java/com/example/reco/LoginActivity.kt
+++ app/src/main/java/com/example/reco/LoginActivity.kt
@@ -5,20 +5,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
 import android.widget.ImageButton
 
-class LoginOrJoinActivity : AppCompatActivity() {
+@SuppressLint("MissingInflatedId")
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_or_join)
+        setContentView(R.layout.activity_login)
 
-        val localLoginBtn = findViewById<ImageButton>(R.id.localLoginBtn)
+        val backBtn = findViewById<ImageButton>(R.id.LoginBackBtn)
 
-        localLoginBtn.setOnClickListener{
-            val intent = Intent(this,LoginActivity::class.java)
+        backBtn.setOnClickListener {
+            val intent = Intent(this,LoginOrJoinActivity::class.java)
             startActivity(intent)
-            Log.d("LoginOrJoin","move to login[local]")
+            Log.d("Login","move to loginOrJoin")
         }
     }
 }
