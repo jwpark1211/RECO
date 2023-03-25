9c24d35a71577aa6d676d5802851388cc9af0830
jwPark <1211abc@naver.com>
2023-03-25 16:45:11 +0900

    fix : xml 디자인 수정

--- app/src/main/java/com/example/reco/JoinActivity.kt
+++ app/src/main/java/com/example/reco/JoinActivity.kt
@@ -1,22 +1,31 @@
 package com.example.reco
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.ImageButton
 
+@SuppressLint("MissingInflatedId")
 class JoinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
 
         val backBtn = findViewById<ImageButton>(R.id.JoinBackBtn)
-
         backBtn.setOnClickListener {
             val intent = Intent(this,LoginOrJoinActivity::class.java)
             startActivity(intent)
             Log.d("Join","move to loginOrJoin")
+        }
+
+        val joinNextBtn = findViewById<Button>(R.id.JoinNextBtn)
+        joinNextBtn.setOnClickListener {
+            val intent = Intent(this,JoinProfileActivity::class.java)
+            startActivity(intent)
+            Log.d("Join","move to joinProfile")
         }
 
     }
